7eaec614983d0d4d9626df87af08e662417a3a19
Matheus Diedrichs <136713101+MatheusDDChs@users.noreply.github.com>
2024-07-04 20:59:53 -0300

    CREATING/Learning JavaScript

---
+++ Meus Bagulho/aulao01/assets/script.js
@@ -0,0 +1,13 @@
+
+// Tipos de variáveis:
+let idade = 18 // Number
+
+let nome = "Matheus" // String
+
+let logado = false // Boolean
+
+let lista = ["farinha", "agua", "fermento", "açucar"]
+
+for (let item of lista) {
+    console.log(item)
+}
