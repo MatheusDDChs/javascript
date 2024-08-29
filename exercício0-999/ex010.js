b1544778b63f21b00d124c7221910976a8c3ec20
Matheus Diedrichs <136713101+MatheusDDChs@users.noreply.github.com>
2024-08-29 19:24:11 -0300

    Adding some tests of conditions and some Functions

---
+++ exercício0-999/ex010.js
@@ -0,0 +1,8 @@
+vel = 60
+
+console.log(`A velocidade do seu carro ${vel}Km/h`)
+if (vel >= 61){ //Isso é uma condição simples!
+    console.log("Você dirigiu acima da velocidade permitida. MULTADO")
+}   
+    else {
+console.log("Dirija sempre usando o cinto de segurança")}
