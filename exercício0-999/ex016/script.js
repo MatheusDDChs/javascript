ce146a958465f9007491dcb3689b653eaf3d8677
Matheus Diedrichs <matheusdiedrichs72@gmail.com>
2024-09-15 17:27:03 -0300

    Calculadora de Frete simples com CSS completo e uma boa base para futuros projetos

--- exercício0-999/ex016/script.js
+++ exercício0-999/ex016/script.js
@@ -1,13 +1,42 @@
 function calc() {
-    let kim = document.Number.querySelector("#km1")
-    let val = document.Number.querySelector("#num1")
+    //Puxa os elementos do HTML para o JS
+    let kim = document.querySelector("#km1").value
+    let val = document.querySelector("#num1").value
     let resultado = document.querySelector("#resultado")
 
-    let res = kim * val
+    //Alterar as variaveis para valores
+    kmNum = parseFloat(kim)
+    valNum = parseFloat(val)
 
-    if (kim > 0 && kim < 100) {
-        val = 1
-
-        resultado.innerHTML = (`O valor deste frete será de ${res}`)
+    //Verifica se valores são válidos
+    if (isNaN(kmNum) || isNaN(valNum)) {
+        resultado.innerHTML = "Por favor, escolha um número válidos."
+        return false;
     }
+
+    //Cálcula o resultado
+    let res = kmNum * valNum;
+
+    if (kmNum > 0 &&  kmNum < 100) {
+        resultado.innerHTML = `O valor deste frete será de R$ ${res.toFixed(2)}`
+
+    } 
+
+    else if (kmNum >= 100 && kmNum <= 200) {
+        resultado.innerHTML = `Você está utilizando a faixa <strong>2</strong>. O valor deste frete será de R$ ${res.toFixed(2)}`
+    } 
+
+    else if (kmNum > 200 && kmNum <= 300) {
+        resultado.innerHTML = `Você está utilizando a faixa <strong>3</strong>. O valor deste frete será de R$ ${res.toFixed(2)}`
+    }
+
+    else if (kmNum > 300) {
+        resultado.innerHTML = `Você está utilizando a faixa <strong>4</strong>. O valor deste frete será de R$ ${res.toFixed(2)} `
+    }
+
+    else {
+        resultado.innerHTML = "Insira uma distância válida. Insira uma distância entre 1 e 99 Km."
+    }
+
+    return false
 }
