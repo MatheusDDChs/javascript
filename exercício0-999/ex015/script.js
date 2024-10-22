a20182a994e05fe0db1f29e9f411648d01b59aa4
Matheus Diedrichs <136713101+MatheusDDChs@users.noreply.github.com>
2024-10-21 22:38:17 -0300

    trials

--- exercício0-999/ex015/script.js
+++ exercício0-999/ex015/script.js
@@ -5,11 +5,9 @@ function verificar() {
     let data1 = document.querySelector("#data1")
     let res = document.querySelector("#pres")
 
-        if (data1.value.length == 0 || Number(data1.value) > nyear){
+        if (data1.value.length == 0 || data1.value > nyear){
             window.alert("[ERRO] Verifique os dados e tente novamente!")
         } else {
-            let fsex  = document.getElementById('radsex')
-            let idade = nyear - Number(data1.value)
-            res.innerHTML = `idade calculada: ${idade}`
+            window.alert("DEU CERTO!!!")
         }
 }
